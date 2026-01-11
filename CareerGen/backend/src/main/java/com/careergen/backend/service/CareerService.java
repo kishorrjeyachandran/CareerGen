2ebfd8b79c81bc60eedafc5f63a75bf0108e3cb1
kishorrjeyachandran <kishor.r.jeyachandran@gmail.com>
2026-01-11 19:41:02 +0530

    Delete CareerGen directory

--- CareerGen/backend/src/main/java/com/careergen/backend/service/CareerService.java
+++
@@ -1,9 +0,0 @@
-package com.careergen.backend.service;
-
-import java.util.List;
-
-import com.careergen.backend.model.CareerResponse;
-
-public interface CareerService {
-    CareerResponse recommendCareer(List<String> skills);
-}
